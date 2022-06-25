bb4ed351ce1766e702bd134f7c1cc81d09aa2923
Ali <44875945+alikdb@users.noreply.github.com>
2022-06-25 10:21:13 +0300

    Update badge.php

--- badge.php
+++ badge.php
@@ -1,1 +1,7 @@
-
+<?php 
+$apiUrl = 'https://api.habboapi.net/badges?per_page=50'; 
+$badges = json_decode(file_get_contents($api_url), true); 
+foreach($badges['data'] as $badge){
+	// Echo out badges
+	echo '<img src="https://images.habbo.com/c_images/album1584/' . $badge['code'] . '.gif" />'; 
+}
