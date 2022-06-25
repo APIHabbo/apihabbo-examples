8a0b5bdf5d3f89916188d1ff04de270cc1e28bdb
Ali <44875945+alikdb@users.noreply.github.com>
2022-06-25 10:20:13 +0300

    Update badge.js

--- badge.js
+++ badge.js
@@ -1,1 +1,9 @@
-
+$(function(){
+	// Get Badges from our API
+	$.getJSON('https://apihabbo.com/api/badges?per_page=50', function(badges){
+		$.each(badges.data, function(key, badge){
+			// Append badges to div
+			$('#badges').append('<img src="https://images.habbo.com/c_images/album1584/' + badge.code + '.gif" />');	
+		});
+	});
+})
